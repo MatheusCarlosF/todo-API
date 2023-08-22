2522c230843ea77e4fa933022dadacabdd05da84
MatheusCarlosF <matheuscarlos16@hotmail.com>
2023-08-22 20:06:43 -0300

    first

---
+++ src/router/api.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as ApiController from "../controllers/apiControllers";
+
+const router = Router()
+
+router.get('/todo', ApiController.all)
+router.post('/todo', ApiController.add)
+router.put('/todo/:id', ApiController.update)
+router.delete('/todo/:id',ApiController.remove)
+
+
+export default router
