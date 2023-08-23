30ed53fdf93bd53b70f4951fee9f18f7303de2eb
MatheusCarlosF <matheuscarlos16@hotmail.com>
2023-08-22 21:26:22 -0300

    test

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
+// export const sequelize = new Sequelize(
+//     process.env.MYSQL_DB as string,
+//     process.env.MYSQL_USER as string,
+//     process.env.MYSQL_PASSWORD as string,
+//     {
+//         dialect: "mysql",
+//         port: parseInt(process.env.MYSQL_PORT as string)
+//     }
+// )
+
 export const sequelize = new Sequelize(
-    process.env.MYSQL_DB as string,
-    process.env.MYSQL_USER as string,
-    process.env.MYSQL_PASSWORD as string,
-    {
-        dialect: "mysql",
-        port: parseInt(process.env.MYSQL_PORT as string)
-    }
+    "mysql -hcontainers-us-west-65.railway.app -uroot -pKzrXHR3RVny7nagkSIZ5 --port 5682 --protocol=TCP railway"
 )
